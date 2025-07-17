fa985159962b3bd11ede63968052411b26956354
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2025-07-17 16:45:02 +0530

    Added Header & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-function App() {
+import { useEffect } from 'react';
+
+import Lenis from 'lenis'
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Footer from './components/footer/Footer';
+
+
+import './App.css'
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {    
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(lenis.raf);
+      lenis.destroy();
+    };
+  }, []);
 
 
   return (
     <>
-     
+    <main className="site_flex flex_column site_gap"> 
+
+     <Footer />
+    </main>
     </>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,36 @@
+import ampLogo from "/amp-footer-logo.svg";
+import linkIcon from "/anchor-ico.svg";
+import "./footer.css";
 
-
-export default function Footer(){
-    return(
-        <>
-        <footer id="site_footer">
-            
-        </footer>
-        </>
-    )
+function Footer() {
+  return (
+    <>
+      <footer id="site_footer">
+        <div className="site_container">
+          <div className="site_flex">
+            <div className="footer_left">
+              <div className="footer_left-inner site_flex flex_column ">
+                <img src={ampLogo} alt="Amp Media Logo" />
+                <div className="footer_email">
+                  <p>Talk to us</p>
+                  <a href="mailto:info@theampmedia.com" >
+               
+                    <span class="anchor_hover"> info@theampmedia.com </span>
+                    <span>
+                      <img src={linkIcon} alt="Link Icon" />
+                    </span>
+                  </a>
+                </div>
+                <p>© 2025 AMPV Media Pvt. Ltd. All rights earned, not given.</p>
+              </div>
+            </div>
+            <div className="footer_right">
+                
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
+export default Footer;
