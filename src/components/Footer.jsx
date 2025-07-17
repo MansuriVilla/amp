a3215b514a9f7e4ab09793b8166ae328fd27bb26
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2025-07-17 15:46:57 +0530

    Added Header & Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer(){
+    return(
+        <>
+        <footer id="site_footer">
+            
+        </footer>
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Header(){
+    return(
+        <>
+        <header id="site_header">
+            <nav className="header_nav">
+                
+            </nav>
+        </header>
+        </>
+    )
+}
